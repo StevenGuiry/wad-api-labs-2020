85af07449f9abc341c03fd439895329a4cccecd2
46968144 <46968144+StevenGuiry@users.noreply.github.com>
2020-12-03 13:43:04 +0000

    Week 9 Error Handling

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -3,20 +3,24 @@ import {
   getMovies, getMovie, getMovieReviews
 } from '../tmdb-api';
 
-const router = express.Router(); //took out 'next' from all three get functions
+const router = express.Router(); //took out 'next' from the first two get functions
 
 router.get('/', (req, res, next) => {
-  getMovies().then(movies => res.status(200).send(movies));
+  getMovies().then(movies => res.status(200).send(movies))
+  .catch((error) => next(error));
 });
 
 router.get('/:id', (req, res, next) => {
   const id = parseInt(req.params.id);
-  getMovie(id).then(movie => res.status(200).send(movie));
+  getMovie(id).then(movie => res.status(200).send(movie))
+  .catch((error) => next(error));
 });
 
 router.get('/:id/reviews', (req, res, next) => {
   const id = parseInt(req.params.id);
-  getMovieReviews(id).then(reviews => res.status(200).send(reviews));
+  getMovieReviews(id)
+  .then(reviews => res.status(200).send(reviews))
+  .catch((error) => next(error));
 });
 
 export default router;
