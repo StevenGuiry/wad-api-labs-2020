41461f44f68774d943f860248d9242249297a35e
46968144 <46968144+StevenGuiry@users.noreply.github.com>
2020-12-03 14:57:03 +0000

    Validation rules added, adding getGenres is WIP

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import {
-  getMovies, getMovie, getMovieReviews
+  getMovies, getMovie, getMovieReviews, getGenres
 } from '../tmdb-api';
 
 const router = express.Router(); //took out 'next' from the first two get functions
 
 router.get('/', (req, res, next) => {
   getMovies().then(movies => res.status(200).send(movies))
+  .catch((error) => next(error));
+});
+
+router.get('/:genre_id', (req, res, next) => {
+  const genre_id = parseInt(req.params.genre_ids);
+  getGenres(genre_id).then(movies => res.status(200).send(movies))
   .catch((error) => next(error));
 });
 
